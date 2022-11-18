6e430aac759b024132af3fddca8ee538a561aebf
Aksel Kvitberg <aksel.kvitberg@outlook.com>
2022-11-18 14:50:59 +0100

    2016 Day 8

--- 2016/util.fsx
+++ 2016/util.fsx
@@ -38,3 +38,5 @@ let (|Regex|_|) pattern input =
     let m = Regex.Match(input, pattern)
     if m.Success then Some(List.tail [ for g in m.Groups -> g.Value])
     else None
+
+let (|Int|_|) (str:string) = match Int32.TryParse str with true, value -> Some value | _ -> None
